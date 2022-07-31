840f0a31c1cbf3bb22b96b1c74777836ea577dd5
Tilekbai <kali@kali.kali>
2022-07-31 13:31:02 +0600

    Add libraries and apps

--- school/settings.py
+++ school/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd app libraries
+    'phonenumber_field',
+    #my apps
+    'accounts', 
+    'webapp',
 ]
 
 MIDDLEWARE = [
